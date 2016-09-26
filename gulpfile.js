5fcf61738782be34c5c68e1fd3a44f7310cf83c2
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-09-25 21:05:33 -0600

    generated if/else test

--- gulpfile.js
+++ gulpfile.js
@@ -13,24 +13,53 @@ var gulp          = require('gulp'),
     flatten       = require('gulp-flatten'),
     regexReplace  = require('gulp-regex-replace'),
     gutil         = require('gulp-util'),
+    gulpif        = require('gulp-if'),
     config        = require('./config.json'),
     paths         = {
       tokens: config.path.tokens,
       dist: config.path.dist,
       temp: config.path.temp,
       assets: config.path.assets
-    };
+    },
+
+    jsonCss       = require('gulp-json-css');
 
 require('require-dir')('./gulp');
 
-// Clean Build
 gulp.task('clean-build', function() {
   gulp.src( paths.dist ).pipe(clean());
 });
 
-gulp.task('default', [
+gulp.task('generate-tokens', [
   'clean-build', 
   'json-stylus-stylesheet',
   'json-ios-color', 
   'iconography'
 ]);
+
+function myFunction() {
+  return gulp
+    .src([
+      paths.tokens + '/**/*.json',
+      '!' + paths.tokens + '/**/styles.json'
+    ])
+  .pipe(jsonCss({
+    targetPre: "scss",
+    delim: "-"
+  }))
+  .pipe(rename({
+    prefix: "_"
+  }))
+  .pipe(replace('%', '$'))
+}
+  
+
+gulp.task('test', ['clean-build'], function() {
+  return gulp
+    .src([
+      paths.tokens + '/**/*.json',
+      '!' + paths.tokens + '/**/styles.json'
+    ])
+    .pipe(gulpif(config.languages.sass === true, myFunction()))
+    .pipe(gulp.dest( paths.dist + '/scss'));
+});
